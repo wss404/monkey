084a48be00706e91bf60825241f63cc82b3ad1bd
staphaness@163.com <staphaness@163.com>
2022-11-17 21:08:38 +0800

    添加双字符词法单元

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,7 +21,11 @@ if (5 < 10) {
 	return true;
 } else {
 	return false;
-}`
+}
+
+10 == 10;
+10 != 9;
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -91,6 +95,17 @@ if (5 < 10) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		//10 == 10;
+		//10 != 9;
+
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
