f69831e515b1af27c1b370b7ceea87b51a598de0
staphaness@163.com <staphaness@163.com>
2023-01-02 22:46:34 +0800

    4.2.1 在词法分析器中支持字符串

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -73,6 +73,10 @@ func (l *Lexer) NextToken() (tok token.Token) {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
+
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
@@ -117,6 +121,17 @@ func (l *Lexer) readNumber() string {
 	position := l.position
 	for isDigit(l.ch) {
 		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
 	}
 	return l.input[position:l.position]
 }
--- token/token.go
+++ token/token.go
@@ -12,8 +12,9 @@ const (
 	EOF     = "EOF"
 
 	// 标识符 + 字面量
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT"
+	IDENT  = "IDENT" // add, foobar, x, y, ...
+	INT    = "INT"
+	STRING = "STRING"
 
 	// 运算符
 	ASSIGN   = "="
