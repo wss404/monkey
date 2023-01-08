1d7e25bb339d4c17157a45f559fdef8004f51064
staphaness@163.com <staphaness@163.com>
2023-01-08 20:56:13 +0800

    5.4 unquote

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -165,7 +165,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return &object.Function{Parameters: params, Env: env, Body: body}
 	case *ast.CallExpression:
 		if node.Function.TokenLiteral() == "quote" {
-			return quote(node.Arguments[0])
+			return quote(node.Arguments[0], env)
 		}
 		function := Eval(node.Function, env)
 		if isError(function) {
@@ -470,10 +470,6 @@ func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Obje
 	}
 
 	return &object.Hash{Pairs: pairs}
-}
-
-func quote(node ast.Node) object.Object {
-	return &object.Quote{Node: node}
 }
 
 func newError(format string, a ...interface{}) *object.Error {
