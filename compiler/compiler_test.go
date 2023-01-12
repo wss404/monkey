4d9af5491d4a6d4e95a0ebc2df90ef6e589e23c2
staphaness@163.com <staphaness@163.com>
2023-01-12 22:12:42 +0800

    补充测试用例

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -19,6 +19,16 @@ type compilerTestCase struct {
 func TestIntegerArithmetic(t *testing.T) {
 	tests := []compilerTestCase{
 		{
+			input:             "1 + 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpAdd),
+				code.Make(code.OpPop),
+			},
+		},
+		{
 			input:             "1; 2",
 			expectedConstants: []interface{}{1, 2},
 			expectedInstructions: []code.Instructions{
