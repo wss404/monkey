22cdb1a34dffda13ea0bc98a885148dbf3e5469c
staphaness@163.com <staphaness@163.com>
2023-01-17 22:45:38 +0800

    6.3 哈希表

--- vm/vm.go
+++ vm/vm.go
@@ -117,6 +117,21 @@ func (vm *VM) Run() error {
 				return err
 			}
 
+		case code.OpHash:
+			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			hash, err := vm.buildHash(vm.sp-numElements, vm.sp)
+			if err != nil {
+				return err
+			}
+			vm.sp = vm.sp - numElements
+
+			err = vm.push(hash)
+			if err != nil {
+				return err
+			}
+
 		case code.OpTrue:
 			err := vm.push(True)
 			if err != nil {
@@ -298,6 +313,26 @@ func (vm *VM) buildArray(startIndex, endIndex int) object.Object {
 	}
 
 	return &object.Array{Elements: elements}
+}
+
+func (vm *VM) buildHash(startIndex, endIndex int) (object.Object, error) {
+	hashPairs := make(map[object.HashKey]object.HashPair)
+
+	for i := startIndex; i < endIndex; i += 2 {
+		key := vm.stack[i]
+		value := vm.stack[i+1]
+
+		pair := object.HashPair{Key: key, Value: value}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return nil, fmt.Errorf("unusable as hash key: %s", key.Type())
+		}
+
+		hashPairs[hashKey.HashKey()] = pair
+	}
+
+	return &object.Hash{Pairs: hashPairs}, nil
 }
 
 func isTruthy(obj object.Object) bool {
