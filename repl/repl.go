ee755559879e910a43ed18c20f4f0c83281445b4
staphaness@163.com <staphaness@163.com>
2023-01-21 22:19:35 +0800

    8.4 执行内置函数

--- repl/repl.go
+++ repl/repl.go
@@ -19,6 +19,9 @@ func Start(in io.Reader, out io.Writer) {
 	constants := []object.Object{}
 	globals := make([]object.Object, vm.GlobalsSize)
 	symbolTable := compiler.NewSymbolTable()
+	for i, v := range object.Builtins {
+		symbolTable.DefineBuiltin(i, v.Name)
+	}
 
 	for {
 		_, err := fmt.Fprintf(out, PROMPT)
